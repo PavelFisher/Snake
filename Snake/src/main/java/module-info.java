9fd03bc6ec048e4a09b0d116dc3637eca59ad25e
PavelFisher <125215270+PavelFisher@users.noreply.github.com>
2025-03-19 10:49:57 +0300

    Delete Snake directory

--- Snake/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.snake {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens com.example.snake to javafx.fxml;
-    exports com.example.snake;
-}
