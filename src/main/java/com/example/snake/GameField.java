07123de8643377cd8e65fd52e3e3fc7d78ee0c84
PavelFisher <p.kharchenk@yandex.ru>
2025-04-01 18:04:45 +0300

    Add option "pause"

--- src/main/java/com/example/snake/GameField.java
+++ src/main/java/com/example/snake/GameField.java
@@ -5,34 +5,28 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
+import java.io.*;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 
-//
-//import javafx.fxml.FXML;
-//import javafx.scene.control.Label;
 
-/// /
-
-
-public class GameField extends JPanel implements ActionListener {
+public class GameField extends JPanel implements ActionListener, Serializable {
     private final int COUNT = 20;
     private int sizeX = 20; // default count elements
     private int sizeY = 20;
-    private final int DOT_SIZE = 16;
-    private final int TECH_HEIGHT = 58;
-    private final int TECH_WIDTH = 40;
+    private final int DOT_SIZE = 30;
+    private final int TECH_HEIGHT = 92;
+    private final int TECH_WIDTH = 54;
     private int currentHeight = sizeY * DOT_SIZE + TECH_HEIGHT;
     private int currentWidth = sizeX * DOT_SIZE + TECH_WIDTH;
-    private BufferedImage apple;
+    private transient BufferedImage apple;
     private int appleX;
     private int appleY;
     private final ArrayList<Integer> x = new ArrayList<>();
     private final ArrayList<Integer> y = new ArrayList<>();
-    private final ArrayList<int[]> rotateElement = new ArrayList<>();
-    private final ArrayList<BufferedImage> images = new ArrayList<>();
+    private transient ArrayList<BufferedImage> images = new ArrayList<>();
     private int dots;
     private Timer timer;
     private boolean left = false;
@@ -41,8 +35,43 @@ public class GameField extends JPanel implements ActionListener {
     private boolean down = false;
     private boolean inGame = true;
     private boolean pause = false;
-    private BufferedImage imageBack;
-    private GameOver GameOver;
+    private transient BufferedImage imageBack;
+    private transient BufferedImage head;
+    private transient BufferedImage body;
+    private transient BufferedImage tail;
+    private transient Popup Popup;
+
+    public GameField() {
+        setForeground(Color.GRAY);
+        setBackground(Color.black);
+        initGame();
+    }
+
+    public void stop() {
+        timer.stop();
+    }
+
+    public void setPause() {
+        this.pause = true;
+    }
+
+    public int getCurrentHeight() {
+        return currentHeight;
+    }
+
+    public int getCurrentWidth() {
+        return currentWidth;
+    }
+
+    public void saveGame() {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("src/saves/saves.dat"))) {
+            oos.writeObject(this);
+            System.out.println("File has been written");
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
 
     public void onChangeSize(Dimension dimension) {
         boolean exit = false;
@@ -63,36 +92,41 @@ public class GameField extends JPanel implements ActionListener {
         currentWidth = dimension.width = sizeX * DOT_SIZE + TECH_WIDTH;
     }
 
-    public GameField() {
-        setForeground(Color.GRAY);
-        setBackground(Color.black);
-        loadImages();
-        initGame();
-        addKeyListener(new FieldKeyListener());
-        setFocusable(true);
-    }
 
     public void initGame() {
-        images.clear();
-        rotateElement.clear();
+        images = null;
+        x.clear();
+        y.clear();
         loadImages();
         dots = 3;
         inGame = true;
-        x.clear();
-        y.clear();
         left = false;
         right = true;
         up = false;
         down = false;
         for (int i = 0; i < dots; i++) {
-            x.add(i, 48 - i * DOT_SIZE);
-            y.add(i, 48);
+            x.add(i, dots * DOT_SIZE - i * DOT_SIZE);
+            y.add(i, dots * DOT_SIZE);
         }
         if (timer == null) {
             timer = new Timer(350, this);
             timer.start();
         } else timer.restart();
         createApple();
+        addKeyListener(new FieldKeyListener());
+        setFocusable(true);
+    }
+
+    public void loadGame() {
+        loadImages();
+        inGame = true;
+        pause = false;
+        if (timer == null) {
+            timer = new Timer(350, this);
+            timer.start();
+        } else timer.restart();
+        addKeyListener(new FieldKeyListener());
+        setFocusable(true);
     }
 
     public void createApple() {
@@ -108,11 +142,14 @@ public class GameField extends JPanel implements ActionListener {
     public void loadImages() {
         try {
             imageBack = ImageIO.read(new File("src/assets/green_grass.jpg"));
-            images.add(ImageIO.read(new File("src/assets/head.png")));
-            images.add(ImageIO.read(new File("src/assets/body.png")));
-            images.add(ImageIO.read(new File("src/assets/tail.png")));
-
-            apple = ImageIO.read(new File("src/assets/apple.png"));
+            head = ImageIO.read(new File("src/assets/head30v2.png"));
+            body = ImageIO.read(new File("src/assets/body30.png"));
+            tail = ImageIO.read(new File("src/assets/tail30.png"));
+            apple = ImageIO.read(new File("src/assets/apple30.png"));
+            if (images == null) images = new ArrayList<>();
+//            images.add(head);
+//            images.add(body);
+//            images.add(tail);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -120,7 +157,9 @@ public class GameField extends JPanel implements ActionListener {
 
     @Override
     protected void paintComponent(Graphics g) {
-        int[] element;
+        int angle;
+        BufferedImage image;
+        Font font;
         super.paintComponent(g);
 
         Image resultingImage = imageBack.getScaledInstance(currentWidth, currentHeight, Image.SCALE_DEFAULT);
@@ -128,43 +167,59 @@ public class GameField extends JPanel implements ActionListener {
         outputImage.getGraphics().drawImage(resultingImage, 0, 0, null);
         g.drawImage(outputImage, 0, 0, this);
 
+        font = new Font("Arial", Font.PLAIN, 36);
+        g.setFont(font);
+        int width = currentWidth - 50;
+        g.drawString(String.valueOf(dots - 3), currentWidth - 50 - 20 * (int) (Math.log10(dots - 3)), 40);
+
+
         if (pause) {
-            Font font = new Font("Arial", Font.BOLD, 22);
+            font = new Font("Arial", Font.BOLD, 22);
             g.setFont(font);
             g.drawString("Pause", currentWidth / 2 - 60, currentHeight / 2);
         } else if (inGame) {
             g.drawImage(apple, appleX, appleY, this);
-            for (int i = 0; i < dots; i++) { //from head to tail
-//                rotate picture
+////                rotate picture
+            for (int i = 0; i < x.size(); i++) {
+//                0 element already rotated
+                if (i == x.size() - 1) image = tail;
+                else if (i == 0) image = head;
+                else image = body;
+                if (i != 0) {
+                    if (x.get(i) == x.get(i - 1)) {
+                        if (y.get(i) > y.get(i - 1)) {
+                            angle = -90;
+                        } else {
+                            angle = 90;
+                        }
+                    } else {
+                        if (x.get(i) > x.get(i - 1)) angle = 180;
+                        else angle = 0;
+                    }
+                } else {
+                    if (x.get(i) == x.get(i + 1)) {
+                        if (y.get(i) > y.get(i + 1)) {
+                            angle = 90;
+                        } else {
+                            angle = -90;
+                        }
+                    } else {
+                        if (x.get(i) > x.get(i + 1)) angle = 0;
+                        else angle = 180;
+                    }
+                }
+                if (images.size() - 1 < i) images.add(i, rotateImage(image, angle));
+                else images.set(i, rotateImage(image, angle));
                 g.drawImage(images.get(i), x.get(i), y.get(i), this);
-                for (int[] ints : rotateElement) {
-                    if (ints[0] == i)
-                        images.set(i, rotateImage(images.get(i), ints[1]));
-                }
-            }
-            for (int i = 0; i < rotateElement.size(); i++) {
-                element = rotateElement.get(i);
-                if (element[0] == dots) rotateElement.remove(i);
-                else {
-                    element[0] += 1;
-                    rotateElement.set(i, element);
-                }
             }
         }
     }
 
     public void move() {
-        int prevValue = 0;
         for (int i = dots - 1; i > 0; i--) {
             if (x.size() <= i) {
                 x.add(x.get(i - 1));
-                images.add(1, images.get(1));
-                for (int j = 0; j < rotateElement.size(); j++) {
-                    int[] ints = rotateElement.get(j);
-                    if (ints[0] == 1) continue;
-                    ints[0] += 1;
-                    if (ints[0] >= dots) rotateElement.remove(j);
-                }
+                images.add(1, body);
             } else x.set(i, x.get(i - 1));
 
             if (y.size() <= i) y.add(y.get(i - 1));
@@ -223,17 +278,18 @@ public class GameField extends JPanel implements ActionListener {
                 repaint();
             } else repaint();
         } else {
-            if (GameOver == null) {
-                GameOver = new GameOver();
-                GameOver.askContinue();
+            if (Popup == null) {
+                Popup = new Popup();
+                Popup.askContinue();
             }
-            if (GameOver.getContinue()) {
+            if (Popup.getContinue()) {
                 initGame();
-                GameOver.dispose();
-                GameOver = null;
+                Popup.dispose();
+                Popup = null;
             }
         }
     }
+
 
     class FieldKeyListener extends KeyAdapter {
         @Override
@@ -244,11 +300,9 @@ public class GameField extends JPanel implements ActionListener {
             if (key == KeyEvent.VK_LEFT && !right) {
                 if (up) {
                     images.set(0, rotateImage(images.getFirst(), -90));
-                    rotateElement.addFirst(new int[]{1, -90});
                 }
                 if (down) {
                     images.set(0, rotateImage(images.getFirst(), 90));
-                    rotateElement.addFirst(new int[]{1, 90});
                 }
                 left = true;
                 up = false;
@@ -257,11 +311,9 @@ public class GameField extends JPanel implements ActionListener {
             if (key == KeyEvent.VK_RIGHT && !left) {
                 if (down) {
                     images.set(0, rotateImage(images.getFirst(), -90));
-                    rotateElement.addFirst(new int[]{1, -90});
                 }
                 if (up) {
                     images.set(0, rotateImage(images.getFirst(), 90));
-                    rotateElement.addFirst(new int[]{1, 90});
                 }
                 right = true;
                 up = false;
@@ -271,11 +323,9 @@ public class GameField extends JPanel implements ActionListener {
             if (key == KeyEvent.VK_UP && !down) {
                 if (right) {
                     images.set(0, rotateImage(images.getFirst(), -90));
-                    rotateElement.addFirst(new int[]{1, -90});
                 }
                 if (left) {
                     images.set(0, rotateImage(images.getFirst(), 90));
-                    rotateElement.addFirst(new int[]{1, 90});
                 }
                 right = false;
                 up = true;
@@ -284,17 +334,16 @@ public class GameField extends JPanel implements ActionListener {
             if (key == KeyEvent.VK_DOWN && !up) {
                 if (right) {
                     images.set(0, rotateImage(images.getFirst(), 90));
-                    rotateElement.addFirst(new int[]{1, 90});
                 }
                 if (left) {
                     images.set(0, rotateImage(images.getFirst(), -90));
-                    rotateElement.addFirst(new int[]{1, -90});
                 }
                 right = false;
                 down = true;
                 left = false;
             }
         }
+
 
         @Override
         public void keyTyped(KeyEvent e) {
@@ -337,7 +386,7 @@ public class GameField extends JPanel implements ActionListener {
 
         graphics.translate((nWidth - width) / 2, (nHeight - height) / 2);
         // rotation around the center point
-        graphics.rotate(radian, (double) (width / 2), (double) (height / 2));
+        graphics.rotate(radian, (width / 2), (height / 2));
         graphics.drawImage(buffImage, 0, 0, null);
         graphics.dispose();
 
